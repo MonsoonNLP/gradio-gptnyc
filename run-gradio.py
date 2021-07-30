f9b67d68cf02ce93ac70663300b938958fdc26d9
Nick Doiron <ndoiron@mapmeld.com>
2021-07-29 20:07:09 -0800

    update model type and tensor

--- run-gradio.py
+++ run-gradio.py
@@ -1,14 +1,18 @@
 import gradio as gr
-from transformers import AutoModel, GPT2Tokenizer
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 tokenizer = GPT2Tokenizer.from_pretrained("monsoon-nlp/gpt-nyc")
-model = AutoModel.from_pretrained("monsoon-nlp/gpt-nyc", pad_token_id=tokenizer.eos_token_id)
+model = GPT2LMHeadModel.from_pretrained("monsoon-nlp/gpt-nyc", pad_token_id=tokenizer.eos_token_id)
 
 def hello(question, context):
     inp = question + ' - ' + context + ' %%'
-    input_ids = tokenizer.encode(inp)
+    input_ids = torch.tensor([tokenizer.encode(inp)])
     output = model.generate(input_ids, max_length=50, early_stopping=True)
-    return tokenizer.decode(output[0], skip_special_tokens=True)
+    resp = tokenizer.decode(output[0], skip_special_tokens=True)
+    if '%%' in resp:
+        resp = resp[resp.index('%%') + 2 : ]
+    return resp
 
 io = gr.Interface(fn=hello,
     inputs=[
