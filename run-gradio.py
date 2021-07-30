bee2605887830ce7e1b3cd157267b60c1fb6954d
Nick Doiron <ndoiron@mapmeld.com>
2021-07-29 18:05:26 -0800

    include inputs

--- run-gradio.py
+++ run-gradio.py
@@ -1,10 +1,11 @@
 import gradio as gr
-from transformers import AutoModel, AutoTokenizer
+from transformers import AutoModel, GPT2Tokenizer
 
 tokenizer = GPT2Tokenizer.from_pretrained("monsoon-nlp/gpt-nyc")
 model = AutoModel.from_pretrained("monsoon-nlp/gpt-nyc", pad_token_id=tokenizer.eos_token_id)
 
-def hello(input):
+def hello(question, context):
+    inp = question + ' - ' + context + ' %%'
     input_ids = tokenizer.encode(inp)
     beam_output = model.generate(input_ids, max_length=100, num_beams=5,
         no_repeat_ngram_size=2, early_stopping=True)
@@ -12,12 +13,15 @@ def hello(input):
     return ".".join(output.split(".")[:-1]) + "."
 
 io = gr.Interface(fn=hello,
-    inputs='text',
-    outputs='text',
+    inputs=[
+        gr.inputs.Textbox(label="Question"),
+        gr.inputs.Textbox(lines=3, label="Context"),
+    ],
+    outputs=gr.outputs.Textbox(label="Reply"),
     verbose=True,
     title='GPT-NYC Input',
-    description='Format chars: Question? - Details %%',
-    thumbnail='https://github.com/MonsoonNLP/gradio-gptnyc',
+    description='Learn more at https://monsoonnlp.com/gpt-nyc/index.html',
+    #thumbnail='https://github.com/MonsoonNLP/gradio-gptnyc',
     analytics_enabled=True)
 
 io.launch(debug=True)
