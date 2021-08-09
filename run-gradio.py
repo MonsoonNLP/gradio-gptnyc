03509941d309618c469879ca1cc43bd36e5055c6
Nick Doiron <ndoiron@mapmeld.com>
2021-08-08 19:05:38 -0800

    options for gpt-nyc small nontoxic

--- run-gradio.py
+++ run-gradio.py
@@ -5,17 +5,34 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 tokenizer = GPT2Tokenizer.from_pretrained("monsoon-nlp/gpt-nyc")
 model = GPT2LMHeadModel.from_pretrained("monsoon-nlp/gpt-nyc", pad_token_id=tokenizer.eos_token_id)
 
-def hello(question, context):
-    inp = question + ' - ' + context + ' %%'
-    input_ids = torch.tensor([tokenizer.encode(inp)])
-    output = model.generate(input_ids, max_length=50, early_stopping=True)
-    resp = tokenizer.decode(output[0], skip_special_tokens=True)
+tagged_tokenizer = GPT2Tokenizer.from_pretrained("monsoon-nlp/gpt-nyc-nontoxic")
+tagged_model = GPT2LMHeadModel.from_pretrained("monsoon-nlp/gpt-nyc-nontoxic", pad_token_id=tokenizer.eos_token_id)
+
+
+def hello(mtype, question, context):
+    if mtype == 'Main':
+        inp = question + ' - ' + context + ' %%'
+        input_ids = torch.tensor([tokenizer.encode(inp)])
+        output = model.generate(input_ids, max_length=50, early_stopping=True)
+        resp = tokenizer.decode(output[0], skip_special_tokens=True)
+    else:
+        if mtype == 'Toxic':
+            inp = question + ' - ' + context + ' %% <Toxic>'
+        else:
+            inp = question + ' - ' + context + ' %% <NonToxic>'
+        input_ids = torch.tensor([tagged_tokenizer.encode(inp)])
+        output = tagged_model.generate(input_ids, max_length=50, early_stopping=True)
+        resp = tagged_tokenizer.decode(output[0], skip_special_tokens=True)
+
     if '%%' in resp:
         resp = resp[resp.index('%%') + 2 : ]
+    if 'Toxic>' in resp:
+        resp = resp[resp.index('Toxic>') + 6 : ]
     return resp
 
 io = gr.Interface(fn=hello,
     inputs=[
+	gr.inputs.Radio(choices=["Main", "NonToxic", "Toxic"], type="value", default="Main", label="Model"),
         gr.inputs.Textbox(label="Question"),
         gr.inputs.Textbox(lines=3, label="More Details (optional)"),
     ],
